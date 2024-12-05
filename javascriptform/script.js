af379956cedc566c1eda5040ae963c802dcc7a2d
waifan321 <wfan002@campus.goldsmiths.ac.uk>
2024-12-05 15:40:03 +0000

    Update script.js

--- javascriptform/script.js
+++ javascriptform/script.js
@@ -17,33 +17,6 @@ function validateAllFields() {
     return; // Ensures the function exits after all validations are passed
 }
 
-function checkFirstname() {
-<<<<<<< Updated upstream
-    const firstname = document.getElementById('forename').value.trim();
-
-    if (firstname === "") {
-        throw new Error('Please enter your first name');
-=======
-    const firstname = document.getElementById('name').value.trim();
-
-    if (firstname === "") {
-        throw new Error('Please enter a valid first name');
->>>>>>> Stashed changes
-    }
-}
-
-function checkSurname() {
-    const surname = document.getElementById('surname').value.trim();
-
-    if (surname === "") {
-<<<<<<< Updated upstream
-        throw new Error('Please enter your surname');
-=======
-        throw new Error('Please enter a valid surname');
->>>>>>> Stashed changes
-    }
-}
-
 function checkEmail() {
     const email = document.getElementById('email').value.trim();
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
