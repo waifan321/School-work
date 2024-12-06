8e41acd4565ecd90b485c612b74b7e6710ae5742
waifan321 <wfan002@campus.goldsmiths.ac.uk>
2024-12-06 22:02:08 +0000

    cleanup changes

--- javascriptform/script.js
+++ javascriptform/script.js
@@ -1,12 +1,9 @@
 document.getElementById('submitBtn').addEventListener('click', function() {
+    event.preventDefault(); // Prevents the default form submission
     try {
         validateAllFields(); // Call the main validation function
-<<<<<<< HEAD
         document.getElementById('message').innerText = "All inputs are valid.";
         return; // Ensures the function exits after all validations are passed
-=======
-        document.getElementById('message').innerText = "All inputs are valid."; // Display success message
->>>>>>> e33fc8f0facbfb8f179728ca932ec4aa0fb042c6
     } catch (error) {
         document.getElementById('message').innerText = error.message; // Display the specific error message
     }
@@ -17,9 +14,28 @@ function validateAllFields() {
     checkSurname();
     checkEmail()
     checkAge();
-    
+    checkDateofbirth();
     return; // Ensures the function exits after all validations are passed
 }
+
+function checkFirstname() { 
+    const firstname = document.getElementById('firstname').value.trim();
+    const namePattern = /^[a-z ,.'-]+$/i;
+
+    if (firstname === "" || !namePattern.test(firstname)) {
+        throw new Error('Please enter a valid first name');
+    }
+}
+
+function checkSurname() {
+    const surname = document.getElementById('surname').value.trim();
+    const namePattern = /^[a-zA-Z]{2,}$/;
+
+    if (surname === "" || !namePattern.test(surname)) {
+        throw new Error('Please enter a valid surname');
+    }
+}
+
 function checkEmail() {
     const email = document.getElementById('email').value.trim();
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -35,7 +51,16 @@ function checkAge() {
     if (age === "" || isNaN(age)) {
         throw new Error('Please enter a valid age');
     } else if (age < 18) {
-        throw new Error('You are too young to register');
+        throw new Error('You must be 18 years or older to submit this form');
     }
 }  
+
+function checkDateofbirth() {
+    const dateofbirth = document.getElementById('date').value.trim();
+    const dateofbirthPattern = /^\d{4}-\d{2}-\d{2}$/;
+
+    if (dateofbirth === "" || !dateofbirthPattern.test(dateofbirth)) {
+        throw new Error('Please enter a valid date of birth');
+    }
+}
 
